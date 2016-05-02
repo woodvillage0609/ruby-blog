cce6338db7acd365835d2484f63038d54e86b043
woodvillage0609 <woodvillage0609@gmail.com>
2016-05-02 21:44:57 +0100

    registrations controller put back

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations"}
   resources :categories
   
   root 'notes#index'
