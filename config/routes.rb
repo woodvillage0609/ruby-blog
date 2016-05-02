829c94f93fed761bd1be038702f06ce26368ca9a
woodvillage0609 <woodvillage0609@gmail.com>
2016-05-02 21:33:05 +0100

    registrations controller removed

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
-  devise_for :users, controllers: { registrations: "registrations"}
+  devise_for :users
   resources :categories
   
   root 'notes#index'
