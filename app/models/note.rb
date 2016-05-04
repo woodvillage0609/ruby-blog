9c44ceb44f38875b32cccc57a50fdf4e8526eba4
woodvillage0609 <woodvillage0609@gmail.com>
2016-05-04 21:47:28 +0100

    s3 try

--- app/models/note.rb
+++ app/models/note.rb
@@ -8,7 +8,7 @@ class Note < ActiveRecord::Base
 		:storage => :s3,
     	:s3_credentials => "#{Rails.root}/config/s3.yml",
     	:path => ":attachment/:id/:style.:extension"
-    	
+  
 	validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 	belongs_to :user
