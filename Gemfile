247187cf9729a6bf45c58290aac10c860964767e
woodvillage0609 <woodvillage0609@gmail.com>
2016-05-04 22:11:03 +0100

    s3 trying

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'paperclip'
 
 gem 'rails_12factor', '~> 0.0.3', group: :production
 
-gem 'aws-sdk'
+gem 'aws-sdk', '~> 2.3'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/note.rb
+++ app/models/note.rb
@@ -4,11 +4,8 @@ class Note < ActiveRecord::Base
 	validates :content, presence:true
 	validates :category, presence:true
 
-	has_attached_file :photo, 
-		:storage => :s3,
-    	:s3_credentials => "#{Rails.root}/config/s3.yml",
-    	:path => ":attachment/:id/:style.:extension"
-  
+	has_attached_file :photo
+
 	validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 	belongs_to :user
